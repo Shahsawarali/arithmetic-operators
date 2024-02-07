d906d1fbe550b3fdf4572a0ae0162d68baa35826
IlOvecompUter1$t <shahva2022@gmail.com>
2024-02-08 00:56:55 +0500

    operators

---
+++ arithmetic.js
@@ -0,0 +1,31 @@
+//Arithmetic operator
+
+
+// let a = 3;
+// let b = 2;
+// console.log("a = ",a,"b = ",b);
+// console.log("a + b =", a+b);//addition
+// console.log("a - b =", a-b);//subtraction
+// console.log("a * b =", a*b);//multiplication
+// console.log("a / b =", a/b);//division
+// console.log("a % b =", a%b);//modules use for remainder
+// console.log("a ** b =", a**b);//exponentiation use for power
+
+
+
+
+//Unary Operators is increment operator and decrement operator
+let a = 3;
+let b = 2;
+// increment operator
+console.log("a = ",a,"b = ",b);
+// a--;// a = a - 1
+// console.log("a =",a);
+
+
+// post and pre decrement and increment
+console.log("a++",a++);
+console.log(a)
+
+console.log("--a",--a);
+console.log(a)
